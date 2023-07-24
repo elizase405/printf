d98bb1f60475e175ec4991c858effa49c5e6aeae
eliza <elizase405@gmail.com>
2023-07-24 15:40:55 -0700

    Fix betty errora

--- _printf.c
+++ _printf.c
@@ -12,36 +12,44 @@
 
 int _printf(const char * const format, ...)
 {
-convert_match m[] = {
-{"%s", print_string}, {"%c", print_char},
-{"%i", printf_int},{"%d", printf_dec},
-};
+	convert_match m[] = {
+		{"%s", print_string},
+		{"%c", print_char},
+		{"%%", print_char},
+		{"%i", printf_int},
+		{"%d", printf_dec},
+	};
 
-va_list args;
-int i = 0, j, len = 0;
+	va_list args;
+	int i = 0, j, len = 0;
 
-va_start(args, format);
-if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-return (-1);
+	va_start(args, format);
 
-Here:
-while (format[i] != '\0')
-{
-j = 3;
-while (j >= 0)
-{
-if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-{
-len += m[j].f(args);
-i = i + 2;
-goto Here;
-}
-j--;
-}
-_putchar(format[i]);
-len++;
-i++;
-}
-va_end(args);
-return (len);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	while (format[i] != '\0')
+	{
+		j = 4;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				if (format[i] == '%' && format[i + 1] == '%')
+					i++;
+				else
+				{
+					len += m[j].f(args);
+					i = i + 2;
+				}
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(args);
+
+	return (len);
 }
--- func1.c
+++ func1.c
@@ -4,46 +4,47 @@
 
 /**
  * print_char - prints given character
- * @c: character to be printed
+ * @args: character to be printed
  *
  * Return: void
  */
 
 int print_char(va_list args)
 {
-char s;
+	char s;
 
-s = va_arg(args, int);
-_putchar(s);
-return (1);
+	s = va_arg(args, int);
+	_putchar(s);
+	return (1);
 }
 
 /**
  * print_string - prints given character
- * @str: string to be printed
+ * @args: string to be printed
  *
  * Return: void
  */
 
 int print_string(va_list args)
 {
-char *s;
-int i, len;
+	char *s;
+	int i, len;
 
-s = va_arg(args, char *);
-if (s == NULL)
-{
-s = "(null)";
-len = _strlen(s);
-for (i = 0; i < len; i++)
-_putchar(s[i]);
-return (len);
-}
-else
-{
-len = _strlen(s);
-for (i = 0; i < len; i++)
-_putchar(s[i]);
-return (len);
-}
+	s = va_arg(args, char *);
+
+	if (s == NULL)
+	{
+		s = "(null)";
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+	else
+	{
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
 }
