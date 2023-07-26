dce6399dd3a62af48c163ac3fa4990d4f75aed6b
nsohpeter <apiahpetertoch@gmail.com>
2023-07-26 14:43:21 -0700

    task 5

--- _printf.c
+++ _printf.c
@@ -18,7 +18,8 @@ int _printf(const char * const format, ...)
 		{"%%", print_char},
 		{"%i", printf_int},
 		{"%d", printf_dec},
-		{"%b", print_binary}
+		{"%b", print_binary},
+		{"%S", printf_non_printable_string},
 	};
 
 	va_list args;
@@ -31,7 +32,7 @@ int _printf(const char * const format, ...)
 
 	while (format[i] != '\0')
 	{
-		j = 5;
+		j = 6;
 		while (j >= 0)
 		{
 			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
